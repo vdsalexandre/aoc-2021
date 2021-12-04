0314f254c30904102e04105876a12d6c4a711750
vdsalex <alexandre.vandensteen@gmail.com>
2021-12-04 16:44:23 +0100

    init

---
+++ src/test/kotlin/SonarSweepTest.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SonarSweepTest {
+
+    @Test
+    internal fun `should do something`() {
+        assertThat(true).isTrue
+    }
+}
