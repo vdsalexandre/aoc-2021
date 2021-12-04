0db0323409e3823aea6db4f3ac19cd9997e9a0ce
vdsalex <alexandre.vandensteen@gmail.com>
2021-12-04 17:46:58 +0100

    part 2

--- src/main/kotlin/SonarSweep.kt
+++ src/main/kotlin/SonarSweep.kt
@@ -11,4 +11,17 @@ class SonarSweep {
 
         return count
     }
+
+    fun countIncreasesMeasurementBy3(measures: IntArray): Int {
+        var count = 0
+        var elementToCompare = measures[0] + measures[1] + measures[2]
+
+        for (i in 1 until measures.size - 2) {
+            val measuresBy3 = measures[i] + measures[i+1] + measures[i+2]
+            if (measuresBy3 > elementToCompare)
+                count++
+            elementToCompare = measuresBy3
+        }
+        return count
+    }
 }
--- src/test/kotlin/SonarSweepTest.kt
+++ src/test/kotlin/SonarSweepTest.kt
@@ -4,17 +4,29 @@ import org.junit.jupiter.api.Test
 
 class SonarSweepTest {
 
+    private val path: String = "src/test/resources/sonarsweep.txt"
+
     @Test
     internal fun `should count increases measurement`() {
         val expectedCount = 1266
-        val measures = readMeasuresFromFile("src/test/resources/sonarsweep.txt").toIntArray()
+        val measures = readMeasuresFromFile()
 
         val sonar = SonarSweep()
         val count = sonar.countIncreasesMeasurement(measures)
-        print("count : $count")
 
         assertThat(count).isEqualTo(expectedCount)
     }
 
-    private fun readMeasuresFromFile(filename: String): List<Int> = File(filename).useLines { it.map { it.toInt() }.toList() }
+    @Test
+    internal fun `part 2`() {
+        val expectedCount = 1217
+        val measures = readMeasuresFromFile()
+
+        val sonar = SonarSweep()
+        val count = sonar.countIncreasesMeasurementBy3(measures)
+
+        assertThat(count).isEqualTo(expectedCount)
+    }
+
+    private fun readMeasuresFromFile(): IntArray = File(path).useLines { it.map { it.toInt() }.toList() }.toIntArray()
 }
